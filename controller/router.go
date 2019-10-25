254b7200160f7e68dc64c6f1f5444fecfae178e3
Felipe Sulzbach <felipesulzsbach_datum@getnet.com.br>
2019-10-25 15:20:00 -0300

    Refactored the create  routers.

--- controller/router.go
+++ controller/router.go
@@ -20,49 +20,52 @@ func CreateRouters(routerWS *mux.Router) {
 	log.Println("ROUTERS Creating...")
 
 	createRoutersCourse(routerWS)
-
-	subRouterCourse := routerWS.PathPrefix("/course").Subrouter()
-	subRouterCourse.Path("").HandlerFunc(service.FindAllCourse).Methods("GET").Name("FindAllCourse")
-	subRouterCourse.Path("/{id}").HandlerFunc(service.FindByIDCourse).Methods("GET").Name("FindByIDCourse")
-	subRouterCourse.Path("").HandlerFunc(service.InsertCourse).Methods("POST").Name("InsertCourse")
-	subRouterCourse.Path("").HandlerFunc(service.UpdateCourse).Methods("PUT").Name("UpdateCourse")
-	subRouterCourse.Path("/{id}").HandlerFunc(service.DeleteCourse).Methods("DELETE").Name("DeleteCourse")
-
-	subRouterClass := routerWS.PathPrefix("/class").Subrouter()
-	subRouterClass.Path("").HandlerFunc(service.FindAllClass).Methods("GET").Name("FindAllClass")
-	subRouterClass.Path("/{id}").HandlerFunc(service.FindByIDClass).Methods("GET").Name("FindByIDClass")
-	subRouterClass.Path("").HandlerFunc(service.InsertClass).Methods("POST").Name("InsertClass")
-	subRouterClass.Path("").HandlerFunc(service.UpdateClass).Methods("PUT").Name("UpdateClass")
-	subRouterClass.Path("/{id}").HandlerFunc(service.DeleteClass).Methods("DELETE").Name("DeleteClass")
-
-	subRouterPerson := routerWS.PathPrefix("/person").Subrouter()
-	subRouterPerson.Path("").HandlerFunc(service.FindAllPerson).Methods("GET").Name("FindAllPerson")
-
-	subRouterStudent := routerWS.PathPrefix("/student").Subrouter()
-	subRouterStudent.Path("").HandlerFunc(service.FindAllStudent).Methods("GET").Name("FindAllStudent")
-	subRouterStudent.Path("/{id}").HandlerFunc(service.FindByIDStudent).Methods("GET").Name("FindByIDStudent")
-	subRouterStudent.Path("").HandlerFunc(service.InsertStudent).Methods("POST").Name("InsertStudent")
-	subRouterStudent.Path("").HandlerFunc(service.UpdateStudent).Methods("PUT").Name("UpdateStudent")
-	subRouterStudent.Path("/{id}").HandlerFunc(service.DeleteStudent).Methods("DELETE").Name("DeleteStudent")
-
-	subRouterTeacher := routerWS.PathPrefix("/teacher").Subrouter()
-	subRouterTeacher.Path("").HandlerFunc(service.FindAllTeacher).Methods("GET").Name("FindAllTeacher")
-	subRouterTeacher.Path("/{id}").HandlerFunc(service.FindByIDTeacher).Methods("GET").Name("FindByIDTeacher")
-	subRouterTeacher.Path("").HandlerFunc(service.InsertTeacher).Methods("POST").Name("InsertTeacher")
-	subRouterTeacher.Path("").HandlerFunc(service.UpdateTeacher).Methods("PUT").Name("UpdateTeacher")
-	subRouterTeacher.Path("/{id}").HandlerFunc(service.DeleteTeacher).Methods("DELETE").Name("DeleteTeacher")
-
+	createRoutersClass(routerWS)
+	createRoutersPerson(routerWS)
+	createRoutersStudent(routerWS)
+	createRoutersTeacher(routerWS)
 	http.Handle("/", routerWS)
 
 	log.Printf("ROUTERS Successfully created on: http://%s/", Domain)
 }
 
-// CreateRoutersCourse - Creates routes to Course endpoints.
 func createRoutersCourse(routerWS *mux.Router) {
-	subRouterCourse := routerWS.PathPrefix("/course").Subrouter()
-	subRouterCourse.Path("").HandlerFunc(service.FindAllCourse).Methods("GET").Name("FindAllCourse")
-	subRouterCourse.Path("/{id}").HandlerFunc(service.FindByIDCourse).Methods("GET").Name("FindByIDCourse")
-	subRouterCourse.Path("").HandlerFunc(service.InsertCourse).Methods("POST").Name("InsertCourse")
-	subRouterCourse.Path("").HandlerFunc(service.UpdateCourse).Methods("PUT").Name("UpdateCourse")
-	subRouterCourse.Path("/{id}").HandlerFunc(service.DeleteCourse).Methods("DELETE").Name("DeleteCourse")
+	subRouter := routerWS.PathPrefix("/course").Subrouter()
+	subRouter.Path("").HandlerFunc(service.FindAllCourse).Methods("GET").Name("FindAllCourse")
+	subRouter.Path("/{id}").HandlerFunc(service.FindByIDCourse).Methods("GET").Name("FindByIDCourse")
+	subRouter.Path("").HandlerFunc(service.InsertCourse).Methods("POST").Name("InsertCourse")
+	subRouter.Path("").HandlerFunc(service.UpdateCourse).Methods("PUT").Name("UpdateCourse")
+	subRouter.Path("/{id}").HandlerFunc(service.DeleteCourse).Methods("DELETE").Name("DeleteCourse")
+}
+
+func createRoutersClass(routerWS *mux.Router) {
+	subRouter := routerWS.PathPrefix("/class").Subrouter()
+	subRouter.Path("").HandlerFunc(service.FindAllClass).Methods("GET").Name("FindAllClass")
+	subRouter.Path("/{id}").HandlerFunc(service.FindByIDClass).Methods("GET").Name("FindByIDClass")
+	subRouter.Path("").HandlerFunc(service.InsertClass).Methods("POST").Name("InsertClass")
+	subRouter.Path("").HandlerFunc(service.UpdateClass).Methods("PUT").Name("UpdateClass")
+	subRouter.Path("/{id}").HandlerFunc(service.DeleteClass).Methods("DELETE").Name("DeleteClass")
+}
+
+func createRoutersPerson(routerWS *mux.Router) {
+	subRouter := routerWS.PathPrefix("/person").Subrouter()
+	subRouter.Path("").HandlerFunc(service.FindAllPerson).Methods("GET").Name("FindAllPerson")
+}
+
+func createRoutersStudent(routerWS *mux.Router) {
+	subRouter := routerWS.PathPrefix("/student").Subrouter()
+	subRouter.Path("").HandlerFunc(service.FindAllStudent).Methods("GET").Name("FindAllStudent")
+	subRouter.Path("/{id}").HandlerFunc(service.FindByIDStudent).Methods("GET").Name("FindByIDStudent")
+	subRouter.Path("").HandlerFunc(service.InsertStudent).Methods("POST").Name("InsertStudent")
+	subRouter.Path("").HandlerFunc(service.UpdateStudent).Methods("PUT").Name("UpdateStudent")
+	subRouter.Path("/{id}").HandlerFunc(service.DeleteStudent).Methods("DELETE").Name("DeleteStudent")
+}
+
+func createRoutersTeacher(routerWS *mux.Router) {
+	subRouter := routerWS.PathPrefix("/teacher").Subrouter()
+	subRouter.Path("").HandlerFunc(service.FindAllTeacher).Methods("GET").Name("FindAllTeacher")
+	subRouter.Path("/{id}").HandlerFunc(service.FindByIDTeacher).Methods("GET").Name("FindByIDTeacher")
+	subRouter.Path("").HandlerFunc(service.InsertTeacher).Methods("POST").Name("InsertTeacher")
+	subRouter.Path("").HandlerFunc(service.UpdateTeacher).Methods("PUT").Name("UpdateTeacher")
+	subRouter.Path("/{id}").HandlerFunc(service.DeleteTeacher).Methods("DELETE").Name("DeleteTeacher")
 }
